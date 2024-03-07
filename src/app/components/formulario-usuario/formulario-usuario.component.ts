45e2833d17e8c703c8b37468904a9679d210737b
Jhulia Fermo Fascin <jhuliafermo@gmail.com>
2024-03-07 17:29:01 -0300

    validade CPF

--- src/app/components/formulario-usuario/formulario-usuario.component.ts
+++ src/app/components/formulario-usuario/formulario-usuario.component.ts
@@ -90,6 +90,17 @@ export class FormularioUsuarioComponent implements OnInit {
       return;
     }
     const userFormData: User = this.userForm.value;
+
+    if (!this.validateCPF(this.userForm.get('cpf')!.value)) {
+      const alert = await this.alertController.create({
+        header: 'Erro',
+        message: 'Por favor, insira um CPF válido.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      return;
+    }
+
     let cpfInvalid!: boolean;
     let error!:string;
 
@@ -166,4 +177,26 @@ export class FormularioUsuarioComponent implements OnInit {
     this.userForm.get('productsSoja')?.setValue(false);
     this.userForm.get('productsMilho')?.setValue(false);
   }
+
+  validateCPF(cpf: string): boolean {
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = 11 - (soma % 11);
+    let digitoVerificador1 = resto > 9 ? 0 : resto;
+
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = 11 - (soma % 11);
+    let digitoVerificador2 = resto > 9 ? 0 : resto;
+
+    if (parseInt(cpf.charAt(9)) !== digitoVerificador1 || parseInt(cpf.charAt(10)) !== digitoVerificador2) {
+        return false;
+    }
+
+    return true;
+}
 }
